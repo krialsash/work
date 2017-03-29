05f08d8d9a0c1700d097baf1d4cacb78341b0587
krialsash <krialsash@gmail.com>
2017-03-29 19:33:01 +0300

    Add files via upload

---
+++ forma.php
@@ -0,0 +1,23 @@
+<?php
+
+/**task1*/
+
+echo ('Add your information about problem:')
+
+?>
+
+<form action="post.php" method="post">
+
+    <p>Name: <input type="text" name="name"></p>
+
+    <p>E-mail: <input type="email" name="email"></p>
+
+    <p>Phone: <input type="number" name="phone"></p>
+
+    <p>Description: <input type="text" name="description"></p>
+
+    <p>Date: <input type="text" name="Created_at"</p>
+
+    <p><input type="submit" value="Отправить" /><input type="reset" /></p>
+
+</form>
