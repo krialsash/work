317ded3bd68a44e9235d4af226df7ba372360bdd
krialsash <krialsash@gmail.com>
2017-04-05 16:16:47 +0300

    + foreach

--- read.php
+++ read.php
@@ -8,31 +8,15 @@ echo ('Problem:');
 
 <?php
     $pdo = new PDO('mysql:host=localhost;dbname=forma', 'root', 'ctvbyfhbz');
-    var_dump($pdo);
+
     $sql = "SELECT * FROM article";
     $pdo_statement = $pdo->prepare($sql);
     $pdo_statement->execute();
     $result = $pdo_statement->fetchAll();
-    var_dump($result);
 
+    foreach ($result as &$value){
+     }
 ?>
-
-<p></p>
-<?php
-   // $pdo = new PDO('mysql:host=localhost;dbname=forma', 'root', 'ctvbyfhbz');
-   // $sql = "SELECT name FROM article";
-   // $pdo_statement = $pdo->prepare($sql);
-   // $pdo_statement->execute();
-   // $name = $pdo_statement->fetchAll();
-   // var_dump($name);
-
-   // $id = $pdo_statement->fetchall(["id"]);
-   // $name = $pdo_statement->fetchall('name');
-   // $description = 'description';
-   // $created_at = 'created_at';
-?>
-
-<p></p>
 
 <table>
     <tr>
@@ -41,11 +25,11 @@ echo ('Problem:');
         <td>Description:</td>
         <td>Date:</td>
     </tr>
-    <tr>
-        <td><?php print_r ($result) ?></td>
-        <td><?php echo ('$name') ?></td>
-        <td><?php echo ('$description') ?></td>
-        <td><?php echo ('$created_at') ?></td>
+   <tr>
+        <td><?php echo ($value['id']) ?></td>
+        <td><?php echo ($value['name']) ?></td>
+        <td><?php echo ($value['description']) ?></td>
+        <td><?php echo ($value['created_at']) ?></td>
     </tr>
 </table>
 
