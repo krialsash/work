dd0503c9318b358a7486d30cb9f097b67554f77e
krialsash <krialsash@gmail.com>
2017-04-07 20:34:16 +0300

    +autodate

--- create.php
+++ create.php
@@ -20,19 +20,14 @@ if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['creat
 }
 ?>
 
-<form action="create.php" method="post">
+<form action="create.php" method="post" autocomplete="on">
 
     <p>Name: <input type="text" name="name"></p>
 
     <p>Description: <input type="text" name="description"></p>
 
-    <p>Date: <input type="text" name="created_at"></p>
+    <p>Date: <input type="date" name="created_at" value="<?php echo date('Y-m-d H:i:s', Time());?>" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}"/></p>
 
     <p><input type="submit" value="Отправить" /></p>
 
-</form>
-
-<?php
-$time = date('Y-m-d H:i:s', time());
-var_dump($time);
-?>
+ </form>
