268a18186224bff6ff0007a34348a5596543907d
krialsash <krialsash@gmail.com>
2017-04-08 11:57:01 +0300

    delete

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Delete
+ */
+
+if (isset($_GET['id'])) {
+    // Если вызов произошел из этой формы по методу delete
+    $pdo = new PDO("mysql:host=localhost;dbname=forma", 'root', 'ctvbyfhbz');
+    $sql = "DELETE FROM article WHERE id = :id";
+    $pdo_statement = $pdo->prepare($sql);
+    $pdo_statement->bindParam(':id', $_GET['id']);
+    $Result = $pdo_statement->execute();
+    var_dump($Result);
+
+    header ("Location: read.php");
+    exit(); // возвращаемся в read.php
+}
+?>
